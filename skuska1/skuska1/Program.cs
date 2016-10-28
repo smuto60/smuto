a36e9b52cbd9c9d7489ba0af45cc2d18ea336aa0
marek smutny <mareksmutny60@gmail.com>
2016-10-28 13:30:08 +0200

    kontrola

--- skuska1/skuska1/Program.cs
+++ skuska1/skuska1/Program.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace skuska1
 {
     class Program
     {
+        
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("ahoj");
-            Console.ReadLine();
+            public int AddNumbers(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+
         }
+        int result = AddNumbers(10, 5);
+            Console.WriteLine(result);
+        }
+   
+
+
     }
 }
+
